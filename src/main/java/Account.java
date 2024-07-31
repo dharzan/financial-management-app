e871b4a4bc03c7b568608554b4035647eb0f9d1d
dharzan <dguruparan@beckettcorp.com>
2024-07-31 08:35:25 -0400

    added Account.java and CheckingAccount.java constructors

---
+++ src/main/java/Account.java
@@ -0,0 +1,22 @@
+
+package main.java;
+
+public abstract class Account{
+    
+    protected String accountId;
+    protected String accountName;
+    protected double balance;
+
+
+    public Account(String accountId, String accountName, double balance){
+        this.accountId = accountId;
+        this.accountName = accountName;
+        this.balance = balance;
+    }
+
+  
+
+
+
+}
+
---
+++ src/main/java/CheckingAccount.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class CheckingAccount  extends Account{
+    public CheckingAccount(String accountID, String accountName, double balance){
+            super(accountID,accountName,balance);
+    }
+    
+}
